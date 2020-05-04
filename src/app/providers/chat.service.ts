c79190dd9b7bfc79d231fa73e5ceba5932468819
Jose Illan <jose_illan@hotmail.com>
2020-05-04 23:48:51 +0200

    Create interface and conect elements

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
+import { Mensaje } from '../interface/mensaje.interface';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class ChatService {
 
-  private itemsCollection: AngularFirestoreCollection<any>;
-  public chats: any[] = [];
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
+  public chats: Mensaje[] = [];
 
   constructor( private afs: AngularFirestore ) { }
 
   cargarMensajes(){
-    this.itemsCollection = this.afs.collection<any>('chats');
-    return this.itemsCollection.valueChanges();
+    this.itemsCollection = this.afs.collection<Mensaje>('chats');
+    return this.itemsCollection.valueChanges()
+                               .map( (mensajes: Mensaje[]) => {
+                                 console.log(mensajes);
+                                 this.chats = mensajes;
+                               })
+  }
+
+  // Falta el UID del usuario
+  agregarMensaje( texto:string ){
+
+    let mensaje: Mensaje = {
+      nombre: 'Demo',
+      mensaje: texto,
+      fecha: new Date().getTime()
+    }
+
+    return this.itemsCollection.add( mensaje );
+
   }
 
 }
